2cc291b29141b3c3d1f02bdee58bfd3a35cb1624
Haidar <haidattkj26@gmail.com>
2024-12-22 14:42:17 +0700

    Initial Commit

---
+++ search-movie/search_movieApp.swift
@@ -0,0 +1,17 @@
+//
+//  search_movieApp.swift
+//  search-movie
+//
+//  Created by Haidar Rais on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct search_movieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ search-movieTests/search_movieTests.swift
@@ -0,0 +1,17 @@
+//
+//  search_movieTests.swift
+//  search-movieTests
+//
+//  Created by Haidar Rais on 22/12/24.
+//
+
+import Testing
+@testable import search_movie
+
+struct search_movieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
