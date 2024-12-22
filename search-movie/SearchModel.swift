70baec8a358c33362c2a499865842e7aba200a49
Haidar <haidattkj26@gmail.com>
2024-12-22 16:48:18 +0700

    feat: add search model

--- search-movie/SearchModel.swift
+++ search-movie/SearchModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+// MARK: - Search
+struct Search: Codable {
+    let search: [Movie]?
+    let totalResults, response: String?
+
+    enum CodingKeys: String, CodingKey {
+        case search = "Search"
+        case totalResults
+        case response = "Response"
+    }
+}
+
+// MARK: - Movie
+struct Movie: Codable {
+    let title, year, imdbID: String?
+    let type: TypeEnum?
+    let poster: String?
+
+    enum CodingKeys: String, CodingKey {
+        case title = "Title"
+        case year = "Year"
+        case imdbID
+        case type = "Type"
+        case poster = "Poster"
+    }
+}
+
+enum TypeEnum: String, Codable {
+    case movie
+    case series
+    case episode
+}
